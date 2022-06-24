1064612d140c4596cfc92149e0c54e5ede730d66
Pham Ngoc Vu <phamngcvu21@gmail.com>
2022-06-24 11:04:30 +0700

    add expensesFilter component

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,14 +1,43 @@
 import React from 'react';
+import {v4 as uuidv4} from 'uuid';
 
 import './App.css';
 
+import NewExpense from './components/NewExpense/NewExpense';
 import Expenses from './components/Expenses/Expenses';
 
 const App = () => {
+  const expenses = [
+    {
+        id: uuidv4(),
+        title: "Car Insuranse",
+        date: new Date(2021, 2, 21),
+        amount: 232.3
+    },
+    {
+        id: uuidv4(),
+        title: "Food",
+        date: new Date(2021, 3, 21),
+        amount: 23.3
+    },
+    {
+        id: uuidv4(),
+        title: "Car",
+        date: new Date(2021, 5, 21),
+        amount: 23244.3
+    },
+  ]
+
+  const addExpenseHandler = expense => {
+    console.log("In app js");
+    console.log(expense)
+  }
+
   return (
     <>
       <div>
-        <Expenses/>
+        <NewExpense onAddExpense={addExpenseHandler}/>
+        <Expenses expenses={expenses}/>
       </div>
     </>
   );
