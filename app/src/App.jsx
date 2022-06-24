a6007a872786ce9af3e91bb432966c9e44598f08
Pham Ngoc Vu <phamngcvu21@gmail.com>
2022-06-24 11:30:18 +0700

    add and display new expense

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {v4 as uuidv4} from 'uuid';
 
 import './App.css';
@@ -6,31 +6,34 @@ import './App.css';
 import NewExpense from './components/NewExpense/NewExpense';
 import Expenses from './components/Expenses/Expenses';
 
+const INITIAL_APP = [
+  {
+      id: uuidv4(),
+      title: "Car Insuranse",
+      date: new Date(2021, 2, 21),
+      amount: 232.3
+  },
+  {
+      id: uuidv4(),
+      title: "Food",
+      date: new Date(2021, 3, 21),
+      amount: 23.3
+  },
+  {
+      id: uuidv4(),
+      title: "Car",
+      date: new Date(2021, 5, 21),
+      amount: 23244.3
+  },
+]
+
 const App = () => {
-  const expenses = [
-    {
-        id: uuidv4(),
-        title: "Car Insuranse",
-        date: new Date(2021, 2, 21),
-        amount: 232.3
-    },
-    {
-        id: uuidv4(),
-        title: "Food",
-        date: new Date(2021, 3, 21),
-        amount: 23.3
-    },
-    {
-        id: uuidv4(),
-        title: "Car",
-        date: new Date(2021, 5, 21),
-        amount: 23244.3
-    },
-  ]
+  const [expenses, setExpenses] = useState(INITIAL_APP);
 
   const addExpenseHandler = expense => {
-    console.log("In app js");
-    console.log(expense)
+    setExpenses((prevState) => {
+      return [expense, ...prevState];
+    })
   }
 
   return (
